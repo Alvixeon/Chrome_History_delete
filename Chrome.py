cfe476f46e6313c1512996daf1e71d6877b030d2
Alvixeon <32423825+Alvixeon@users.noreply.github.com>
2018-03-06 12:16:11 -0600

    Add files via upload

---
+++ Chrome.py
@@ -0,0 +1,25 @@
+import getpass,os
+global username
+global PTHC
+
+username = getpass.getuser()
+PTHC = "C:\\Users\\%s\\AppData\\Local\\Google\\Chrome\\User Data\\Default" % username
+
+def Delete_History():
+    cdir = os.getcwd()
+    if cdir != PTHC:
+        os.chdir(PTHC)
+    else:
+        pass
+    PTHCLS = os.listdir(os.getcwd())
+    for i in PTHCLS:
+        if i == "History":
+            try:
+                os.remove(i)
+            except:
+                try:
+                    os.system("taskkill /IM chrome.exe /F")
+                except:
+                    pass
+    
+Delete_History()
